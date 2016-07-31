74bef01f4976ac6ac48fc5850b443c1b80e5c62a
dgamb <dgamb@DESKTOP-INRA58G>
2016-07-31 10:27:23 -0500

    Add project files.

---
+++ Angular2Mvc5Application1/Models/List.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models {
+  public class List {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity), Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<Task> Tasks { get; set; }
+    [NotMapped]
+    public int Count { get; set; }
+    [NotMapped]
+    public int CountEnded { get; set; }
+  }
+}
