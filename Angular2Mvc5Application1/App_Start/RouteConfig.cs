7d44e59e6e1d6a63c4619bd49f99466081419225
Darrell <dgambl00@yahoo.com>
2016-08-05 04:22:53 -0500

    Angular 2 template with real connection string.  Deployment to Azure still a work in progress.

--- Angular2Mvc5Application1/App_Start/RouteConfig.cs
+++ Angular2Mvc5Application1/App_Start/RouteConfig.cs
@@ -9,11 +9,14 @@ namespace Angular2Mvc5Application1 {
   public class RouteConfig {
     public static void RegisterRoutes(RouteCollection routes) {
       routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+      var dataTokens = new RouteValueDictionary();
+      var ns = new string[] { "Angular2Mvc5Application1.Controllers" };
+      dataTokens["Namespaces"] = ns;
 
       routes.MapRoute(
           name: "Default",
           url: "{controller}/{action}/{id}",
-          defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+          defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, ns }
       );
     }
   }
